1d57606bae877c3ef8a7edbd642b09c1d1b04747
henriquequintana <102379902+henriquequintana@users.noreply.github.com>
2024-12-04 22:07:09 -0300

    Add files via upload

---
+++ código/AgendaFisioterapeuta-main/src/main/java/model/Usuario.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author hugol
+ */
+public abstract class Usuario implements Serializable{
+    protected String nome;
+    protected String email;
+    protected String senha;
+    protected LocalDate dataNascimento;
+
+    public Usuario(String nome, String email, String senha, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Usuario() {
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
+    
+    
+    
+}
